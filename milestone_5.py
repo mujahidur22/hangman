a2c3472bc2c8d83665bceaf23caed298829b17b5
Fahmid Rahman <mujahidur22@gmail.com>
2023-11-11 15:19:20 +0000

    Starts the game

--- milestone_5.py
+++ milestone_5.py
@@ -47,7 +47,7 @@ def play_game(word_list):
     #num_lives = 5
     game = Hangman(word_list, num_lives=0)
     while True:
-        if game.num_lives == 0:
+        if game.num_lives == 5:
             print("You lost!")
             print(f"The word was {game.word}")
             break
